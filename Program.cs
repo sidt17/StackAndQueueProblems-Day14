bcea037a52f4d212ca641e82958a084602cdfd83
Siddhant <santoshshrivastava39@gmail.com>
2022-03-18 22:37:15 +0530

    UC-3 Solved

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+
+            foreach (int element in queue)
+            {
+                Console.WriteLine(element);
+            }
+
+          
+           
+
+        }
+    }
+}
